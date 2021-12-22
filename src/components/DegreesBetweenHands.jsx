0cd48a47cc56fbd9c70aeb8c444e37fd04bb4316
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-12-22 11:08:24 -0800

    Add descriptive h2 heading for DegreesBetweenHands component

--- src/components/DegreesBetweenHands.jsx
+++ src/components/DegreesBetweenHands.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import useCurrentDateAndTime from '../hooks/useCurrentDateAndTime';
+
 const DegreesBetweenHands = () => {
+  const { currentDateAndTime } = useCurrentDateAndTime();
+
   return (
     <div>
-      <h1>DegreesBetweenHands</h1>
+      <h2>{`Degrees between hour hand at ${currentDateAndTime.getHours()} and minute hand at ${currentDateAndTime.getMinutes()}:`}</h2>
+      <span></span>
     </div>
   );
 };
